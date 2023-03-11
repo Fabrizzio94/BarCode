0f87b1e4878049134da0432ea7929eb107208ce3
Fabricio <fabriciomega@hotmail.com>
2023-03-11 00:56:05 -0500

    Version estable

--- app/src/main/java/com/example/barcode/MainActivity.kt
+++ app/src/main/java/com/example/barcode/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.barcode
 
 import android.Manifest
-import android.app.Instrumentation.ActivityResult
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,7 @@ import androidx.core.content.ContextCompat
 import com.example.barcode.databinding.ActivityMainBinding
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.client.android.BeepManager
+import com.journeyapps.barcodescanner.BarcodeCallback
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
 import com.journeyapps.barcodescanner.DefaultDecoderFactory
 
@@ -21,8 +23,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var barcodeView: DecoratedBarcodeView? = null
     private var beepManager: BeepManager? = null
+    private var tvTexto: TextView? = null
+    private var lastText: String? = null
     private companion object{
         private const val TAG = "PERMISOS_TAG"
+    }
+
+    private val callback = BarcodeCallback { result ->
+        if (result.text == null || result.text == lastText) {
+            return@BarcodeCallback
+        }
+        lastText = result.text
+        barcodeView!!.setStatusText("")
+        beepManager!!.playBeepSoundAndVibrate()
+//        val imageView = findViewById<ImageView>(R.id.barcodePreview)
+//        imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW))
+        tvTexto = binding.tvLectura
+        tvTexto!!.text = this.lastText
     }
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,11 +50,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun barLaunch() {
+        // permite asignar el view al decorador
+        barcodeView = binding.barcodeScanner
         val formats: Collection<BarcodeFormat> =
             listOf(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39)
+        barcodeView?.setStatusText("Pasa el codigo por favor")
         barcodeView?.barcodeView?.decoderFactory = DefaultDecoderFactory(formats)
-        barcodeView?.initializeFromIntent(this.intent)
-        //barcodeView!!.decodeContinuous(callback)
+        barcodeView?.initializeFromIntent(Intent())
+        barcodeView!!.decodeContinuous(callback)
         beepManager = BeepManager(this)
     }
 
@@ -53,7 +73,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        return barcodeView?.onKeyDown(keyCode, event) ?: super.onKeyDown(keyCode, event)
+        return barcodeView?.onKeyDown(keyCode, event) == true || super.onKeyDown(keyCode, event)
     }
 
     private val requestPermissionLauncher =
@@ -84,7 +104,7 @@ class MainActivity : AppCompatActivity() {
                 Manifest.permission.CAMERA
                 ) -> {
                     // additiona rationale be displayed
-                    Toast.makeText(this,"PERMITIDO",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this,"PERMITIDO",Toast.LENGTH_SHORT).show()
                 run {
                     requestPermissionLauncher.launch(
                         Manifest.permission.CAMERA
